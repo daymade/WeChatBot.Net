4afba0d1feea168d082ae3f7c66a9269081030b5
daymade <daymadev89@gmail.com>
2016-09-23 17:24:54 +0800

    Add: add datetime extension method for get unix time

---
+++ WeChatBot.Net/Util/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace WeChatBot.Net.Util.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>转换为Unix时间戳形式</summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static long ToUnixTime(this DateTime date)
+        {
+            return Convert.ToInt64(Math.Floor((date.ToUniversalTime() - UnixEpoch).TotalSeconds));
+        }
+
+        /// <summary>从Unix时间戳转换为utc</summary>
+        /// <param name="unixTime"></param>
+        /// <returns></returns>
+        public static DateTime FromUnixTime(this long unixTime)
+        {
+            return UnixEpoch.AddSeconds((double) unixTime);
+        }
+
+        /// <summary>当前月份的第一天</summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime FirstDayOfMonth(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, 1);
+        }
+
+        /// <summary>当月的总天数</summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int DaysInMonth(this DateTime value)
+        {
+            return DateTime.DaysInMonth(value.Year, value.Month);
+        }
+
+        /// <summary>当前月份的最后一天</summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime LastDayOfMonth(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, value.DaysInMonth());
+        }
+    }
+}
